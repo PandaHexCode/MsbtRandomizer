4ad1f26036ca7016449fdd883975c318f2b02f38
Nagisa/Panda <49723922+PandaHexCode@users.noreply.github.com>
2024-06-24 14:28:41 +0200

    Added crash log

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,6 +1,14 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using MSBTRando;
 using MSBTRando.Windows;
 
 
-new MainWindow();
+try{
+    new MainWindow();
+}catch (Exception ex){
+    string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+    string filename = $"CrashLog_{timestamp}.txt";
+    string logContent = $"Error Message: {ex.Message}\nStack Trace: {ex.StackTrace}";
+
+    File.WriteAllText(filename, logContent);
+}
